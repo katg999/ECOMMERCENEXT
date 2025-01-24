5afba9967d949dad2f0a5802cfd80872a65cba04
braingilbert <briangilbert@DESKTOP-BOD1Q8N.localdomain>
2025-01-24 15:58:08 +0300

    Initial Commit

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,16 +1,22 @@
+import Link from "next/link";
+
 export default function Nav() {
+  const inactiveLink = "flex gap-1 p-2";
+  const activeLink =
+    inactiveLink + " bg-white text-blue-900 ml-10  rounded-l-lg";
+
   return (
-    <aside className="text-white">
-      <a className="flex">
+    <aside className="text-white p-2 pr-0">
+      <Link href={"/"} className="flex gap-1 mb-4 ml-0 mt-0">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
-          viewBox="0 0 24 24" /* Updated viewBox to match actual icon's size */
+          viewBox="0 0 24 24"
           strokeWidth={1.5}
-          stroke="currentColor"
+          stroke="white"
           className="icon-small"
-          width={16} /* Set width to 16px */
-          height={16} /* Set height to 16px */
+          width={16} // Set width to 16px
+          height={16} // Set height to 16px
         >
           <path
             strokeLinecap="round"
@@ -19,17 +25,19 @@ export default function Nav() {
           />
         </svg>
 
-        <span className="">Ecommerce-admin</span>
-      </a>
-      <nav>
-        <Link href={"/"}>
+        <span className="text-white ml-2">Ecommerce-admin</span>
+      </Link>
+      <nav className="flex-col gap-10">
+        <Link href={"/"} className={activeLink}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+            stroke="white"
+            className="icon-small"
+            width={16} // Set width to 16px
+            height={16} // Set height to 16px
           >
             <path
               strokeLinecap="round"
@@ -37,7 +45,65 @@ export default function Nav() {
               d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
             />
           </svg>
-          Dashboard
+          <span className="ml-2">Dashboard</span>
+        </Link>
+        <Link href={"/products"} className={inactiveLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="white"
+            class="size-6"
+            width={16} // Set width to 16px
+            height={16}
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+            />
+          </svg>
+          Products
+        </Link>
+        <Link href={"/orders"} className={inactiveLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="white"
+            class="size-6"
+            width={16} // Set width to 16px
+            height={16} // Set height to 16px
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 0 1 0 3.75H5.625a1.875 1.875 0 0 1 0-3.75Z"
+            />
+          </svg>
+          Orders
+        </Link>
+        <Link href={"/settings"} className={inactiveLink}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="white"
+            class="size-6"
+            className="icon-small"
+            width={16} // Set width to 16px
+            height={16} // Set height to 16px
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 12a7.5 7.5 0 0 0 15 0m-15 0a7.5 7.5 0 1 1 15 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077 1.41-.513m14.095-5.13 1.41-.513M5.106 17.785l1.15-.964m11.49-9.642 1.149-.964M7.501 19.795l.75-1.3m7.5-12.99.75-1.3m-6.063 16.658.26-1.477m2.605-14.772.26-1.477m0 17.726-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205 12 12m6.894 5.785-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495"
+            />
+          </svg>
+          <span classname="ml-2 mr-2">Settings</span>
         </Link>
       </nav>
     </aside>
--- pages/index.js
+++ pages/index.js
@@ -19,10 +19,12 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-blue-900 min-h-screen">
+    <div className="bg-blue-900 min-h-screen flex">
       <Nav />
 
-      <div>Logged in {session.user.email}</div>
+      <div className="bg-white flex-grow ml-2 mt-2 mr-2 mb-2 rounded-lg p-4">
+        Logged in {session.user.email}
+      </div>
     </div>
   );
 }
